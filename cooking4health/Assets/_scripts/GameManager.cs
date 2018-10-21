5d85b0756724da78f3aaee940031ee3905f69f23
Andy <ajmrkva@gmail.com>
2018-10-21 04:15:19 -0400

    all but the action loops

--- cooking4health/Assets/_scripts/GameManager.cs
+++ cooking4health/Assets/_scripts/GameManager.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	public enum location {Customer, Sink, CuttingBoard, Plate, Serve}
+	public enum Location {Customer, Sink, CuttingBoard, Plate, Serve}
+
+	private bool firstChoice;
+	private Location myLocation;
+	public GameObject recipe;
+	public GameObject inventory;
+	public GameObject recipeButton;
+	public GameObject areaCuttingBoard;
+	public GameObject areaSink;
+	public GameObject areaMix;
+	public GameObject areaPlate;
 
 	// Use this for initialization
 	void Start () {
-		
+		recipe.SetActive(true);
+		inventory.SetActive(false);
+		recipeButton.SetActive(false);
+		firstChoice = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void CheckFirstChoice () {
+		if (firstChoice == false) 
+		{
+			recipeButton.SetActive(true);
+			inventory.SetActive(true);
+		}
+	}
+
+	public void SwitchToSink () 
+	{
+		CheckFirstChoice();
+		recipe.SetActive(false);
+		myLocation = Location.Sink;
+		areaCuttingBoard.SetActive(true);
+	}
+
+	public void SwitchToRecipe ()
+	{
+		if (areaCuttingBoard.active == true)
+		{
+			areaCuttingBoard.SetActive(false);
+		} else if (areaMix.active == true) 
+		{
+			areaMix.SetActive(false);
+		} else if (areaPlate.active == true)
+		{
+			areaPlate.SetActive(false);
+		} else if (areaSink.active == true)
+		{
+			areaSink.SetActive(true);
+		} else if (areaPlate.active == true)
+		{
+			areaPlate.SetActive(true);
+		};
+		recipe.SetActive(true);
+
 	}
 }
