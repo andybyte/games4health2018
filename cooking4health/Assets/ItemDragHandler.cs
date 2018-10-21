01b42c69648f00c978b88db10a96c92dd1b38e17
Lauren Bleyaert <lkb41@pitt.edu>
2018-10-21 00:05:49 -0400

    DragDrop Inventory
    drag and drop images
    SO for food items

---
+++ cooking4health/Assets/ItemDragHandler.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ItemDragHandler : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public GameObject emptiedSlot;
+    public static GameObject itemBeingDragged;
+
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        
+        itemBeingDragged = gameObject;
+        Debug.Log("item being dragged!");
+    }
+
+
+
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.GetTouch(0).position;
+        
+    }
+
+
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = Input.GetTouch(0).position;
+
+    }
+
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ cooking4health/Assets/SO_Item.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu]
+public class SO_Item : ScriptableObject {
+
+    public string foodName;
+    public Sprite foodSprite;
+    public GameObject foodObject;
+
+	
+}
+
+
+/*Inventory architecture from Unity tutorial
+ *https://unity3d.com/learn/tutorials/projects/adventure-game-tutorial/inventory?playlist=44381
+ */
