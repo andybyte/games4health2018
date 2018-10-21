75267aa68f2654dad24165ac18131555f7105a9d
Andy <ajmrkva@gmail.com>
2018-10-21 03:00:52 -0400

    title, customer, recipe, getting to loop is working

---
+++ cooking4health/Assets/_scripts/CustomerAGoButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CustomerAGoButton : MonoBehaviour {
+
+	public string sceneToLoad;
+
+	public void GoButton () 
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
---
+++ cooking4health/Assets/_scripts/TitleStartButton.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleStartButton : MonoBehaviour {
+
+	public string sceneToLoad;
+
+	public void TitleStart () 
+	{
+		SceneManager.LoadScene(sceneToLoad);
+	}
+}
