6673e51973b4779a00278ebf52aec65b6b97bc6b
Andy <ajmrkva@gmail.com>
2018-10-21 01:40:09 -0400

    finished base recipe menu

---
+++ cooking4health/Assets/_scripts/RecipeButton.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RecipeButton : MonoBehaviour {
+
+	public GameObject recipePanel;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void EnableRecipe () {
+		if (recipePanel != null) {
+			if (recipePanel.active == false)
+			{
+				recipePanel.active = true;
+			}
+		}
+	}
+}
